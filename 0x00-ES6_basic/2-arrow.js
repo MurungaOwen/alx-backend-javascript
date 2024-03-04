2750a03d203e740dc13fb59dd26d0c009668ee95
MurungaOwen <owenhood80@gmail.com>
2024-03-04 07:31:37 +0300

    functions

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,11 +1,13 @@
-export default function getNeighborhoodsList() {
-    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
-    const self = this;
-    this.addNeighborhood = (newNeighborhood) => {
-        self.sanFranciscoNeighborhoods.push(newNeighborhood);
-        return self.sanFranciscoNeighborhoods;
+export class getNeighborhoodsList {
+    constructor() {
+        this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+        const self = this;
+        this.addNeighborhood = (newNeighborhood) => {
+            self.sanFranciscoNeighborhoods.push(newNeighborhood);
+            return self.sanFranciscoNeighborhoods;
+        };
     }
-  }
+}
 
 
