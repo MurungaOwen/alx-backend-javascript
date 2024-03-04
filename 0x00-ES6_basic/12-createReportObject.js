ae8297c9ad2b1967c146a88adec47193e76713d2
MurungaOwen <owenhood80@gmail.com>
2024-03-04 10:28:24 +0300

    refactor

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,6 +2,9 @@ export default function createReportObject(employeesList) {
     return {
         allEmployees: {
           ...employeesList
+        },
+        getNumberOfDepartments(employeesList) {
+          return Object.keys(employeesList).length;
         }
     };   
 }
