d0c4e919f4d39c74a2510f99d3930a2dd34c2a1c
MurungaOwen <owenhood80@gmail.com>
2024-03-04 12:09:50 +0300

    redo task 1

---
+++ 2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from "./0x00-ES6_basic/2-arrow";
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
