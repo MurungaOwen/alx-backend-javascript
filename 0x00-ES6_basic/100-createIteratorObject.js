55c031a0352587a9a21bf0d32e873fbd12964de2
MurungaOwen <owenhood80@gmail.com>
2024-03-04 10:39:36 +0300

    100

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,29 @@
+export default function createIteratorObject(report) {
+    const departments = Object.values(report.allEmployees);
+    let deptIndex = 0;
+    let empIndex = 0;
+  
+    const iterator = {
+      next() {
+        if (deptIndex >= departments.length) {
+          return { done: true };
+        }
+  
+        const currentDepartment = departments[deptIndex];
+        if (empIndex >= currentDepartment.length) {
+            deptIndex++;
+          empIndex = 0;
+          return this.next();
+        }
+  
+        const employee = currentDepartment[empIndex];
+        empIndex++;
+        return { value: employee, done: false };
+      },
+      [Symbol.iterator]() {
+        return this;
+      }
+    };
+  
+    return iterator;
+  }
