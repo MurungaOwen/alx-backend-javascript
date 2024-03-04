65f5546f4599b063532e543d8aae1738a139734a
MurungaOwen <owenhood80@gmail.com>
2024-03-04 07:26:33 +0300

    block scopes

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task;
+  var task2;
+
+  task = false;
+  task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
