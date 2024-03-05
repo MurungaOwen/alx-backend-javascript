3b64a0e676344050faa68431ac6858be0e8643d7
MurungaOwen <owenhood80@gmail.com>
2024-03-05 12:18:43 +0300

    modify blocked scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,9 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  // task = false;
-  // task2 = true;
-
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task;
+    task2;
   }
 
   return [task, task2];
