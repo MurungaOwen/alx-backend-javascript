6af648322db8f1e2a5ea480121f302648492e7f1
MurungaOwen <owenhood80@gmail.com>
2024-03-07 10:04:52 +0300

    remove async

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { createUser, uploadPhoto } from "./utils";
 
-export default async function handleProfileSignup() {
+export default function handleProfileSignup() {
     Promise.all([uploadPhoto(), createUser()])
         .then(([photoResult, userResult]) => {
             console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
