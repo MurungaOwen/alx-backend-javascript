2da836f3ff3c25cae35a5c5e9aadaf6a5f6530df
MurungaOwen <owenhood80@gmail.com>
2024-03-08 13:23:10 +0300

    add new line and apply eslint fix to 4

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,9 @@
 export default function returnHowManyArguments(...params) {
   let count = 0;
-  let i;
-  for (i in params) {
-    count += 1;
+  for (let items in params) {
+    if(params.hasOwnProperty(items)) {  
+      count += 1;
+    }
   }
   return count;
 }
