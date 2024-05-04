9e76679c66affcd4117d60f7089d01f6b7012e16
MurungaOwen <owenhood80@gmail.com>
2024-05-04 10:30:24 +0300

    log correct error

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -9,40 +9,39 @@ function countStudents(path) {
       fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
           reject(new Error('Cannot load the database'));
+        } else {
+          const lines = data.trim().split('\n');
+          const results = lines.map((line) => line.split(','));
+          let csTotal = 0;
+          const csStud = [];
+          let swTotal = 0;
+          const swStud = [];
+          results.forEach((val) => {
+            if (val[3].startsWith('CS')) {
+              csTotal += 1;
+              csStud.push(val[0]);
+            } else if (val[3].startsWith('SWE')) {
+              swTotal += 1;
+              swStud.push(val[0]);
+            }
+          });
+          const csArr = [];
+          csStud.forEach((val) => {
+            csArr.push(` ${val}`);
+          });
+
+          const swArr = [];
+          swStud.forEach((val) => {
+            swArr.push(` ${val}`);
+          });
+
+          const msg1 = `Number of students: ${lines.length - 1}\n`;
+          const msg2 = `Number of students in CS: ${csTotal}. List: ${csArr}\n`;
+          const msg3 = `Number of students in SWE: ${swTotal}. List: ${swArr}`;
+          const final = `${msg1}${msg2}${msg3}`;
+          console.log(final);
+          resolve(final);
         }
-        const lines = data.trim().split('\n');
-        const results = lines.map((line) => line.split(','));
-        let csTotal = 0;
-        const csStud = [];
-        let swTotal = 0;
-        const swStud = [];
-        results.forEach((val) => {
-          if (val[3].startsWith('CS')) {
-            csTotal += 1;
-            csStud.push(val[0]);
-          } else if (val[3].startsWith('SWE')) {
-            swTotal += 1;
-            swStud.push(val[0]);
-          } else {
-
-          }
-        });
-        const csArr = [];
-        csStud.forEach((val) => {
-          csArr.push(` ${val}`);
-        });
-
-        const swArr = [];
-        swStud.forEach((val) => {
-          swArr.push(` ${val}`);
-        });
-
-        const msg1 = `Number of students: ${lines.length - 1}\n`;
-        const msg2 = `Number of students in CS: ${csTotal}. List: ${csArr}\n`;
-        const msg3 = `Number of students in SWE: ${swTotal}. List: ${swArr}`;
-        const final = `${msg1}${msg2}${msg3}`;
-        console.log(final);
-        resolve(final);
       });
     });
   });
