f32fe2b963c55afc3cd504e35e8873d6f3ff65df
MurungaOwen <owenhood80@gmail.com>
2024-05-04 11:00:14 +0300

    bode basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,12 @@
-// Function to check if input is being piped
-const isPipedInput = () => !process.stdin.isTTY;
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// If input is not piped, prompt the user
-if (!isPipedInput()) {
-  console.log('Welcome to Holberton School, what is your name?');
-}
-
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (chunk) => {
-  const input = chunk.trim();
-  if (input) {
-    console.log(`Your name is: ${input}`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-if (isPipedInput()) {
-  process.stdin.on('end', () => {
-    console.log('This important software is now closing');
-  });
-}
-
-process.on('SIGINT', () => {
-  console.log('This important software is now closing');
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
