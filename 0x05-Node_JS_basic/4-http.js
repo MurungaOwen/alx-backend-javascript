ea7b2fd131df931549dc562204a0a89ab6a772f1
MurungaOwen <owenhood80@gmail.com>
2024-05-04 10:46:31 +0300

    refactor code

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,20 +1,35 @@
-const { createServer } = require('node:http');
+// const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
+// const port = 1245;
+
+// const app = createServer((req, res) => {
+//   if (req.method === 'GET' && req.url === '/') {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Hello Holberton School!');
+//   } else {
+//     res.writeHead(404, { 'Content-Type': 'text/plain' });
+//     res.end('Not Found');
+//   }
+// });
+
+// app.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// module.exports = app;
+const http = require('http');
+
+const hostname = 'localhost';
 const port = 1245;
 
-const app = createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
 module.exports = app;
